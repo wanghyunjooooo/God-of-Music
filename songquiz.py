127d64ad755ed3c7668c6ea0a359780990a7d90f
wanghyunjooooo <w2324@e-mirim.hs.kr>
2024-11-11 01:33:28 +0900

    정답 화면에 정답 노래 뮤비 썸네일 띄우기 구현

--- songquiz.py
+++ songquiz.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 # 퀴즈 데이터 - 각 문제에 대한 오디오 파일 경로와 정답을 설정
 quiz_data = [
-    {"audio": "/static/audio/Steady.mp3", "answer": "steady"},
-    {"audio": "/static/audio/Dunk Shot.mp3", "answer": "dunk shot"},
+    {"audio": "/static/audio/Steady.mp3", "answer": "steady", "music_video_url": "https://www.youtube.com/embed/IKlkZZv76Ho"},
     # 추가적인 문제를 여기에 추가
 ]
 
@@ -18,20 +17,24 @@ def quiz():
     if score['correct'] + score['wrong'] >= 10:
         return redirect(url_for('results'))
     
-    question = random.choice(quiz_data)
+    question = random.choice(quiz_data)  # 랜덤으로 퀴즈 선택
     return render_template('songquiz.html', question=question)
 
 @app.route('/check_answer', methods=['POST'])
 def check_answer():
     user_answer = request.form.get('answer').strip().lower()
     correct_answer = request.form.get('correct_answer').strip().lower()
+    music_video_url = request.form.get('music_video_url')  # 뮤직비디오 URL 가져오기
+
+    # video_id 추출
+    video_id = music_video_url.split('/embed/')[-1]
 
     if user_answer == correct_answer:
         score['correct'] += 1
-        return render_template('songquiz-correct.html')
+        return render_template('songquiz-correct.html', correct_answer=correct_answer, music_video_url=music_video_url, video_id=video_id)
     else:
         score['wrong'] += 1
-        return render_template('songquiz-wrong.html')
+        return render_template('songquiz-wrong.html', correct_answer=correct_answer)
 
 @app.route('/results')
 def results():
